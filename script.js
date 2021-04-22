5dea04984a415926f5c72ea87be18fe993579576
Connie Hyman <connie2630a@gmail.com>
2021-04-22 15:32:02 -0700

    Add score to canvas

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const rules = document.getElementById("rules");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let score = 0;
+
 // Ball
 const ball = {
   x: canvas.width / 2,
@@ -46,6 +48,13 @@ function drawPaddle() {
 function draw() {
   drawBall();
   drawPaddle();
+  drawScore();
+}
+
+//Draw Score
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
 draw();
