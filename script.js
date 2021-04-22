942a0111fe42c24f96803c361938ba578c7e257a
Connie Hyman <connie2630a@gmail.com>
2021-04-21 22:45:56 -0700

    Draw game ball

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const rulesBtn = document.getElementById("rules-btn");
 const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+drawBall();
 
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 
