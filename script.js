2b1fbdd0a71341fd799cdfd62600792a6e7ba773
Connie Hyman <connie2630a@gmail.com>
2021-04-22 23:22:08 -0700

    Set wall boundaries for game ball

--- script.js
+++ script.js
@@ -100,6 +100,21 @@ function movePaddle() {
   }
 }
 
+//Move ball on canvas
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  //Wall Collicsion (right/left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1;
+  }
+  //Wall Collision (top/bottom)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+}
+
 //Draw everything
 function draw() {
   //clear canvas
@@ -114,6 +129,7 @@ function draw() {
 //Update canvas drawing and animation
 function update() {
   movePaddle();
+  moveBall();
 
   //Call Draw Everything
   draw();
