60fd6a5b8862d0171c3ea613b4b56543dd4e2d45
Connie Hyman <connie2630a@gmail.com>
2021-04-22 23:13:07 -0700

    Add paddle animation

--- script.js
+++ script.js
@@ -102,6 +102,9 @@ function movePaddle() {
 
 //Draw everything
 function draw() {
+  //clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
   drawPaddle();
   drawScore();
@@ -118,6 +121,33 @@ function update() {
   requestAnimationFrame(update);
 }
 
-rulesBtn.addEventListener("click", () => rules.classList.add("show"));
+update();
 
+//Keydown event
+function keyDown(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+//Keyup event
+function keyUp(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    paddle.dx = 0;
+  }
+}
+
+//Keyboard event handlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
+
+//Rules and Close buttons event handlers
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
